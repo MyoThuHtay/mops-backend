f9caea96266b1c57fd269c60f81ce58a8fbead20
mops <61018567+MyoThuHtay@users.noreply.github.com>
2023-01-27 22:52:25 +0630

    test

--- middleware/polygonTokens.js
+++ middleware/polygonTokens.js
@@ -14,49 +14,49 @@ const PolygonTokens = async (address, type) => {
       address,
       chain,
     });
-    // for (let i = 0; i < tokenData.raw.length; i++) {
-    //   let token = await Token.findOne({
-    //     contractAddress: tokenData.raw[i].token_address,
-    //   });
+     for (let i = 0; i < tokenData.raw.length; i++) {
+       let token = await Token.findOne({
+         contractAddress: tokenData.raw[i].token_address,
+       });
 
-    //   if (!token) {
-    //     token = new Token({
-    //       type: "POLYGON",
-    //       name: tokenData.raw[i].name,
-    //       symbol: tokenData.raw[i].symbol,
-    //       decimals: tokenData.raw[i].decimals,
-    //       logo: logo + tokenData.raw[i].token_address + "/logo.png",
-    //       amount:
-    //         +tokenData.raw[i].balance/Math.pow(10, tokenData.raw[i].decimals), // Math.pow(10, tokenData.raw[i].decimals),
-    //       contractAddress: tokenData.raw[i].token_address,
-    //     });
-    //     token = await token.save();
-    //     tokenList.push(token);
-    //   } else {
-    //     token = await Token.findOneAndUpdate(
-    //       {
-    //         contractAddress: tokenData.raw[i].token_address,
-    //       },
-    //       {
-    //         $set: {
-    //           type: "POLYGON",
-    //           name: tokenData.raw[i].name,
-    //           symbol: tokenData.raw[i].symbol,
-    //           decimals: tokenData.raw[i].decimals,
-    //           logo: logo + tokenData.raw[i].token_address + "/logo.png",
-    //           amount:
-    //             +tokenData.raw[i].balance/Math.pow(10, tokenData.raw[i].decimals), 
+       if (!token) {
+         token = new Token({
+           type: "POLYGON",
+           name: tokenData.raw[i].name,
+           symbol: tokenData.raw[i].symbol,
+           decimals: tokenData.raw[i].decimals,
+           logo: logo + tokenData.raw[i].token_address + "/logo.png",
+           amount:
+             +tokenData.raw[i].balance/Math.pow(10, tokenData.raw[i].decimals), // Math.pow(10, tokenData.raw[i].decimals),
+           contractAddress: tokenData.raw[i].token_address,
+         });
+         token = await token.save();
+         tokenList.push(token);
+       } else {
+         token = await Token.findOneAndUpdate(
+           {
+             contractAddress: tokenData.raw[i].token_address,
+           },
+           {
+             $set: {
+               type: "POLYGON",
+               name: tokenData.raw[i].name,
+               symbol: tokenData.raw[i].symbol,
+               decimals: tokenData.raw[i].decimals,
+               logo: logo + tokenData.raw[i].token_address + "/logo.png",
+               amount:
+                 +tokenData.raw[i].balance/Math.pow(10, tokenData.raw[i].decimals), 
     //             //Math.pow(10, tokenData.raw[i].decimals),
-    //           contractAddress: tokenData.raw[i].token_address,
-    //         },
-    //       },
-    //       { upsert: true }
-    //     );
-    //     tokenList.push(token);
-    //   }
-    // }
+               contractAddress: tokenData.raw[i].token_address,
+             },
+           },
+           { upsert: true }
+         );
+         tokenList.push(token);
+       }
+     }
 
-    return tokenData;
+    return tokenList;
   } catch (error) {
     throw new Error(error);
   }
